85345e352997ed99eafd7aa8288d4015c990d050
ren6662 <114849966+ren6662@users.noreply.github.com>
2022-12-11 16:39:20 +0000

    [Add]devise機能実装

--- db/migrate/20221209050313_devise_create_users.rb
+++ db/migrate/20221209050313_devise_create_users.rb
@@ -33,6 +33,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.1]
       # t.datetime :locked_at
 
       t.string :name
+      t.boolean :is_deleted, default: false, null: false
 
       t.timestamps null: false
     end
