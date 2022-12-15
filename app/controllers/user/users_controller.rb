6a2622a0d9c32c5305017f554303e5080ffb72b8
ren6662 <114849966+ren6662@users.noreply.github.com>
2022-12-15 02:38:57 +0000

    フォロー機能修正

--- app/controllers/user/users_controller.rb
+++ app/controllers/user/users_controller.rb
@@ -22,7 +22,7 @@ class User::UsersController < ApplicationController
   private
   
   def user_params
-    params.require(:user).permit(:name, :introduction, :is_deleted)
+    params.require(:user).permit(:name, :introduction, :is_deleted, :profile_image)
   end
   
   def set_user
